62fa226021060b37caf3a32bcd91a284b05c8ac9
dasunthamsh <tunathamash@gmail.com>
2023-06-14 18:26:18 +0530

    inmamoerPOS updated

--- assest/js/js.js
+++ assest/js/js.js
@@ -56,3 +56,11 @@ el.addEventListener('mousedown', function() {
 el.addEventListener('mouseup', function() {
     el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
 })
+
+
+// $(window).on('load', function (){
+//
+//     $('#loader').fadeOut(1000);
+// });
+
+
