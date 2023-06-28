e5a5c504012aea2ce26f97e5680e9c56be33553c
dasunthamsh <tunathamash@gmail.com>
2023-06-28 19:50:38 +0530

    project updated

--- assignment/JS/inMemoryPOS/model/Customer.js
+++ assignment/JS/inMemoryPOS/model/Customer.js
@@ -1,8 +1,32 @@
-function Customer(cusID,cusName,cusSalary,cusAddress){
-    return {
-        id: cusID,
-        name: cusName,
-        salary: cusSalary,
-        address: cusAddress
+function Customer(p_code,p_name,p_address,p_salary) {
+    var code = p_code;
+    var name = p_name;
+    var address = p_address;
+    var salary = p_salary;
+
+    this.setCustomerCode = function (c) {
+        code = c;
+    }
+    this.setCustomerName = function (n) {
+        name = n;
+    }
+    this.setCustomerAddress = function (a) {
+        address = a;
+    }
+    this.setCustomerSalary = function (s) {
+        salary = s;
+    }
+
+    this.getCustomerCode = function () {
+        return code;
+    }
+    this.getCustomerName = function () {
+        return name;
+    }
+    this.getCustomerAddress = function () {
+        return address;
+    }
+    this.getCustomerSalary = function () {
+        return salary;
     }
 }
--- assignment/JS/inMemoryPOS/model/Item.js
+++ assignment/JS/inMemoryPOS/model/Item.js
@@ -1,8 +1,32 @@
-function item(itemCode,itemName,qty,itemPrice){
-    return {
-        code: itemCode,
-        name: itemName,
-        quantity: qty,
-        price: itemPrice
+function Item(p_code,p_name,p_price,p_qty) {
+    var code = p_code;
+    var name = p_name;
+    var price = p_price;
+    var qty = p_qty;
+
+    this.setItemCode = function (c){
+        code = c;
+    }
+    this.setItemName = function (n){
+        name = n;
+    }
+    this.setItemPrice = function (p){
+        price = p;
+    }
+    this.setItemQty = function (q){
+        qty = q;
+    }
+
+    this.getItemCode = function () {
+        return code;
+    }
+    this.getItemName = function () {
+        return name;
+    }
+    this.getItemPrice = function () {
+        return price;
+    }
+    this.getItemQty = function () {
+        return qty;
     }
 }
